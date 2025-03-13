6aa8087b8bb678cf7aeef706935c896b7eceec03
Mysael Ribeiro <mysael.dev@gmail.com>
2025-03-12 23:46:33 -0300

    creating component

--- projects/angular/Curso-de-Angular-Basico/aula-angular/src/app/app.component.ts
+++ projects/angular/Curso-de-Angular-Basico/aula-angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';  // ✅ Importando FormsModule
+import { ContatoComponent } from './pagina/contato/contato.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, // Indica que é um Standalone Component
-  imports: [CommonModule, FormsModule], 
+  imports: [CommonModule, FormsModule, ContatoComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
